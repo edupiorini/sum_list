2614bf4b3f6dab898987276f67ffa4af0d987ade
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-10 04:50:16 -0300

    test defined but still in progress

--- test/sum_list_test.exs
+++ test/sum_list_test.exs
@@ -1,7 +1,15 @@
 defmodule SumListTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  describe "call/1" do
+    test "returns the sum of the given list" do
+      list = [1, 2, 3]
+      response = SumList.call(list)
+
+      expect_response = "banana"
+
+      # executando o test
+      assert response == expect_response
+    end
   end
 end
